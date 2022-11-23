3008149adbd1132eb64206e171cbd56deaae34b3
Guorui Yu <corryguoruiyu@gmail.com>
2022-11-23 17:02:17 +0000

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	new file:   ecommence/jsconfig.json
    	modified:   ecommence/package-lock.json
    	modified:   ecommence/package.json
    	deleted:    ecommence/public/favicon.ico
    	modified:   ecommence/public/index.html
    	deleted:    ecommence/public/logo192.png
    	deleted:    ecommence/public/logo512.png
    	deleted:    ecommence/public/manifest.json
    	modified:   ecommence/src/App.css
    	modified:   ecommence/src/App.js
    	modified:   ecommence/src/App.test.js
    	modified:   ecommence/src/index.js
    	deleted:    ecommence/src/logo.svg

--- ecommence/src/App.js
+++ ecommence/src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import React, { useState } from 'react';
+import { Button, Col, Form, FormControl, FormGroup, FormLabel, Row } from 'react-bootstrap';
+
+function Account() {
+  return (
+    <>
+    <Container className='h1 text-center my-5 account'>Welcome</Container>
+    <Container className='shadow account'>
+      <Row className='p-3 border-bottom text-center'>
+        <Col className='border-end h4'><a href='#'>Login</a></Col>
+        <Col className='h4'><a href='#'>Join in</a></Col>
+      </Row>
+      <Form className='w-50 mx-auto pt-5'>
+        <FormGroup controlId='loginEmail'>
+          <FormLabel>Email Address</FormLabel>
+          <FormControl type='email' className='rounded-0 mb-2' required></FormControl>
+        </FormGroup>
+        <FormGroup controlId='loginPwd'>
+          <FormLabel>Password</FormLabel>
+          <FormControl type='password' className='rounded-0' required></FormControl>
+        </FormGroup>
+        <Button type="submit" className='w-100 mt-4 btn-dark rounded-0'>
+        Submit
+      </Button>
+      </Form>
+      <a href='#' className='d-block text-center pt-3 pb-5'>Forget Password?</a>
+    </Container>
+    </>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Account />
   );
 }
 
